58f720f9c8fd7e3ad9f6446fe136720a1109a962
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-05-10 19:10:51 +0100

    add id attribute to duty, use mocker to ensure id is reset for every test

--- duty.py
+++ duty.py
@@ -1,4 +1,9 @@
+
 class Duty:
+    id_counter = 1
     def __init__(self, name, description=None):
         self.name = name
         self.description = description
+        self.id = Duty.id_counter
+
+        Duty.id_counter += 1
--- test_duty.py
+++ test_duty.py
@@ -5,13 +5,27 @@
 # 
 
 from duty import Duty
+import pytest
+
+@pytest.fixture(autouse=True)
+def reset_id_counter(mocker):
+    mocker.patch.object(Duty, 'id_counter', 1)
 
 def test_duty_has_a_name_when_initiaslised_with_a_name():
     d1 = Duty(name="d1")
     assert d1.name == "d1"
 
 def test_duty_has_a_description_when_initialised_with_a_description_and_a_name():
-    d1 = Duty(name='d1', description="description 1")
+    d1 = Duty(name="d1", description="description 1")
     assert d1.description == "description 1"
+
+def test_duty_has_an_id():
+    d1 = Duty(name="d1", description="description 1")
+    assert d1.id == 1
+
+def test_duty_has_no_duplicate_id():
+    d1 = Duty(name="d1", description = "description 1")
+    d2 = Duty(name="d2", description="description 2")
+    assert d1.id != d2.id
 
     
