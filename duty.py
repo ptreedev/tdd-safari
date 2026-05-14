8405753625023f0224b37b55c231484138cfc59d
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-05-14 10:56:30 +0100

    create repo class and refactor factory for single responsibility

--- duty.py
+++ duty.py
@@ -1,27 +1,40 @@
+from dataclasses import dataclass
+@dataclass
 class Duty:
-    def __init__(self, name, description, duty_id):
-        self.name = name
-        self.description = description
-        self.id = duty_id
+    name: str
+    description: str
+    id: int
 
 class DutyFactory:
     def __init__(self):
         self.id_counter = 1
-        self.duty_names = set()
-        self.description = set()
     
     def create_duty(self, name, description):
+
+        new_duty = Duty(name, description, self.id_counter)
+
+        self.id_counter += 1   
+        
+        return new_duty
+
+#This will handle the state of the duties rather than Factory    
+class DutyRepository:
+    def __init__(self):
+        self.factory = DutyFactory()
+        self.description = set()
+        self.duty_names = set()
+
+    def add(self, name, description):
         if name in self.duty_names:
             raise DuplicateNameError("A duty with this name already exists")
         if description in self.description:
             raise DuplicateDescriptionError("A duty with this description already exists")
-        new_duty = Duty(name, description, self.id_counter)
-
-        self.id_counter += 1
+        
+        duty = self.factory.create_duty(name, description)
         self.duty_names.add(name)
         self.description.add(description)
-        
-        return new_duty
+
+        return duty
     
 class DuplicateNameError(Exception):
     pass
