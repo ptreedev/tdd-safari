6f31a081c6225fbcccf0ebc9d5825bcdd3366281
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-05-17 16:58:46 +0100

    add error handling tests

--- duty_controller.py
+++ duty_controller.py
@@ -1,14 +1,20 @@
-from flask import render_template
-
-from duty import Duty
-
+from flask import render_template, redirect, url_for, flash
+from duty import DuplicateDescriptionError, DuplicateNameError
 
 class DutyController:
     def __init__(self, repo) -> None:
-        self._duties = repo.all()
+        self._repo = repo
 
     def list(self) -> str:
-        return render_template("duties/list.html", duties=self._duties)
+        duties = self._repo.all()
+        return render_template("duties/list.html", duties=duties)
     
     def new_form(self) -> str:
         return render_template("duties/new_form.html")
+    
+    def create_duty(self, name, description):
+        try:
+            self._repo.add(name, description)
+        except(DuplicateNameError, DuplicateDescriptionError) as e:
+            flash(str(e), "error")
+        return redirect(url_for("duties_list"))
--- tests/unit/test_duty_controller.py
+++ tests/unit/test_duty_controller.py
@@ -1,3 +1,4 @@
+from duty import DuplicateDescriptionError, DuplicateNameError
 import pytest
 
 from duty_controller import DutyController
@@ -9,6 +10,7 @@ class FakeRepository:
 
             def all(self):
                 return self._duties
+            
 @pytest.fixture
 def duties():
      return [Duty(name='D1', description='desc 1', id=1)]
@@ -39,4 +41,63 @@ class TestDutyControllerList:
         mock_render.assert_called_once_with("duties/new_form.html")
         assert result == "<html>form</html>"   
 
-    
+    def test_create_duty_calls_add_method_with_name_and_desc(self, mocker):
+        repo = mocker.Mock()
+        controller = DutyController(repo)
+
+        mocker.patch("duty_controller.redirect")
+        mocker.patch("duty_controller.url_for")
+
+        controller.create_duty(name="D1", description="desc 1")
+
+        repo.add.assert_called_once_with("D1", "desc 1")
+
+    def test_create_duty_redirects_to_list_view(self, mocker):
+        repo = mocker.Mock()
+        controller = DutyController(repo)
+
+        mock_redirect = mocker.patch("duty_controller.redirect")
+        mock_url_for = mocker.patch("duty_controller.url_for")
+        mock_url_for.return_value = "/duties"
+        mock_redirect.return_value = "<redirect>"
+
+        result = controller.create_duty(name="D1", description="desc 1")
+        mock_url_for.assert_called_once_with("duties_list")                                    
+        mock_redirect.assert_called_once_with("/duties")                                
+        assert result == "<redirect>" 
+
+    def test_create_duty_flashes_and_redirects_to_list_on_duplicate_name(self, mocker):
+        repo = mocker.Mock()
+        repo.add.side_effect = DuplicateNameError("A duty with this name already exists")
+        controller = DutyController(repo)
+
+        mock_flash = mocker.patch("duty_controller.flash")
+        mock_redirect = mocker.patch("duty_controller.redirect")
+        mock_url_for = mocker.patch("duty_controller.url_for")
+        mock_url_for.return_value = "/duties/new_form"
+        mock_redirect.return_value = "<redirect-new-form>"
+
+        result = controller.create_duty(name="D1", description="desc 1")
+
+        mock_flash.assert_called_once_with("A duty with this name already exists", "error")
+        mock_url_for.assert_called_once_with("duties_list")
+        mock_redirect.assert_called_once_with("/duties/new_form")
+        assert result == "<redirect-new-form>"
+
+    def test_create_duty_flashes_and_redirects_to_list_on_duplicate_description(self, mocker):
+        repo = mocker.Mock()
+        repo.add.side_effect = DuplicateDescriptionError("A duty with this description already exists")
+        controller = DutyController(repo)
+
+        mock_flash = mocker.patch("duty_controller.flash")
+        mock_redirect = mocker.patch("duty_controller.redirect")
+        mock_url_for = mocker.patch("duty_controller.url_for")
+        mock_url_for.return_value = "/duties/new_form"
+        mock_redirect.return_value = "<redirect-new-form>"
+
+        result = controller.create_duty(name="D1", description="desc 1")
+
+        mock_flash.assert_called_once_with("A duty with this description already exists", "error")
+        mock_url_for.assert_called_once_with("duties_list")
+        mock_redirect.assert_called_once_with("/duties/new_form")
+        assert result == "<redirect-new-form>"
