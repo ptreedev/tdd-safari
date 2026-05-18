810419b171f7d72fd5f0a9b860378af28ffb9af7
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-05-18 14:54:01 +0100

    refactor and add final test

--- tests/e2e/test_duties_e2e.py
+++ tests/e2e/test_duties_e2e.py
@@ -10,29 +10,36 @@ def add_duty(page: Page, name="D1", description="Desc 1"):
     page.get_by_label("Description:").fill(description)
     page.get_by_role("button", name="Create").click()
 
-def test_title_is_present_on_page(live_server , page: Page):
+def test_title_is_present_on_page(page: Page):
     page.goto("/duties")
     expect(page).to_have_title(duties_regex)
+
+def test_new_duty_form_renders(page: Page):
+    page.goto("/duties/new")
+    expect(page.get_by_label("Name:")).to_be_visible()
+    expect(page.get_by_label("Description:")).to_be_visible()
+    expect(page.get_by_role("button", name="Create")).to_be_enabled()
+    expect(page.get_by_role("link", name="Cancel")).to_be_enabled()
     
-def test_user_can_add_a_new_duty(live_server, page: Page):
+def test_user_can_add_a_new_duty(page: Page):
     add_duty(page)
 
     expect(page).to_have_title(duties_regex)
     expect(page.get_by_text("D1")).to_be_visible()
     expect(page.get_by_text("Desc 1")).to_be_visible()
 
-def test_user_can_navigate_to_all_pages(live_server, page: Page):
+def test_user_can_navigate_to_all_pages(page: Page):
     page.goto("/")
     expect(page.get_by_text("hello there")).to_be_visible()
     expect(page.get_by_role("link", name="Duties")).to_be_visible()
     page.get_by_role("link", name="Duties").click()
 
-    expect(page).to_have_url(f"{live_server.url()}/duties")
+    expect(page).to_have_url("/duties")
     expect(page).to_have_title(duties_regex)
     expect(page.get_by_role('link', name='Add Duty')).to_be_visible()
     page.get_by_role('link', name='Add Duty').click()
     
-    expect(page).to_have_url(f"{live_server.url()}/duties/new")
+    expect(page).to_have_url("/duties/new")
     expect(page.get_by_label("Name:")).to_be_visible()
     expect(page.get_by_label("Description:")).to_be_visible()
     expect(page.get_by_role("link", name="Cancel")).to_be_enabled()
@@ -40,7 +47,7 @@ def test_user_can_navigate_to_all_pages(live_server, page: Page):
 
     expect(page).to_have_title("Duties")
 
-def test_user_sees_error_message_on_duplicate_duty(live_server, page: Page):
+def test_user_sees_error_message_on_duplicate_duty(page: Page):
     add_duty(page)
 
     expect(page).to_have_title(duties_regex)
